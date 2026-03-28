b12f74be5f3758eb503e4cedc94bf985cdf6b33a
Fernand William <156734117+fernandwill@users.noreply.github.com>
2026-03-28 17:24:53 +0700

    feat: add users and auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnalyticsModule } from './features/analytics/analytics.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AnalyticsModule],
+  imports: [AnalyticsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
