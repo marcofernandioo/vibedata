897859c704f1285d6b6e05a23cc2f7399a82f3a5
Fernand William <156734117+fernandwill@users.noreply.github.com>
2026-03-28 17:31:53 +0700

    fix: move module directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnalyticsModule } from './features/analytics/analytics.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './features/users/users.module';
+import { AuthModule } from './features/auth/auth.module';
 
 @Module({
   imports: [AnalyticsModule, UsersModule, AuthModule],
