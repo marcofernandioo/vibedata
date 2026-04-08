868e5d39d3eb29ab433bb1f1970795ae210891d2
Marco Fernandio <67978617+marcofernandioo@users.noreply.github.com>
2026-04-08 21:46:18 +0700

    Revert "Feat/add auth"

--- backend/src/features/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
-import { UsersService } from './users.service';
-
-@Module({
-  providers: [UsersRepository, UsersService],
-  exports: [UsersService],
-})
-export class UsersModule {}
